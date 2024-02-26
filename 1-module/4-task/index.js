78dfba43c7220cca3bcd30fdeae7217f9d846e72
Viktor Vdovin <vdovin121096@gmail.com>
2024-02-26 05:14:10 +0300

    Tasks 4 and 5 Comleted

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,12 @@
 function checkSpam(str) {
   // ваш код...
+  str = str.toLowerCase();
+  if (str.includes("1xbet") || str.includes("xxx")) {
+    return true;
+  }
+  return false;
 }
+
+console.log(checkSpam("1XbeT now"));
+console.log(checkSpam("free xxxxx"));
+console.log(checkSpam("innocent rabbit"));
--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -1,3 +1,12 @@
 function truncate(str, maxlength) {
   // ваш код...
+  if (str.length > 20) {
+    return str.slice(0, maxlength - 1) + "…";
+  } else {
+    return str;
+  }
 }
+
+truncate("Вот, что мне хотелось бы сказать на эту тему:", 20);
+
+truncate("Всем привет!", 20);
