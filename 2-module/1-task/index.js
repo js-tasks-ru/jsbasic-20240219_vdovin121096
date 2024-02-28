288e814f3e8da653d7760b18ef009d4031b35ea4
Viktor Vdovin <vdovin121096@gmail.com>
2024-02-28 11:31:46 +0300

    Tasks comleted

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,20 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (typeof salaries[key] === "number" && isFinite(salaries[key])) {
+      sum += salaries[key];
+    } else {
+      return sum;
+    }
+  }
+  return sum;
 }
+
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: "December",
+  currency: "USD",
+  isPayed: false,
+};
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,20 @@
 function isEmpty(obj) {
-  // ваш код...
+  if (obj == null) {
+    return true;
+  }
+
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      return false;
+    }
+  }
+  return true;
 }
+
+let schedule = {};
+
+alert(isEmpty(schedule)); // true
+
+schedule["8:30"] = "подъём";
+
+alert(isEmpty(schedule)); // false
